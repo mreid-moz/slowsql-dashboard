27ab43e6feb74972c61eac3eeb919fc784c32dc7
Mark Reid <mreid@mozilla.com>
2014-01-29 16:52:37 -0400

    Progress...

--- slowsql.js
+++ slowsql.js
@@ -3,7 +3,25 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 "use strict";
 
-var slowsql_csv = null;
+var yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+//console.log("Set yesterday to " + yyyymmdd(yesterday));
+
+// Monday is 1, so we adjust offset accordingly
+var dowOffset = (yesterday.getUTCDay() - 1) % 7;
+var thisWeekStart = new Date(yesterday);
+var thisWeekEnd = new Date(yesterday);
+thisWeekStart.setDate(thisWeekStart.getDate() - dowOffset);
+thisWeekEnd.setDate(thisWeekStart.getDate() + (7 - dowOffset));
+//console.log("Set this week to " + yyyymmdd(thisWeekStart) + " to " + yyyymmdd(thisWeekEnd));
+
+var lastWeekStart = new Date(thisWeekStart);
+lastWeekStart.setDate(lastWeekStart.getDate() - 7);
+var lastWeekEnd = new Date(thisWeekEnd);
+lastWeekEnd.setDate(lastWeekEnd.getDate() - 7);
+//console.log("Set last week to " + yyyymmdd(lastWeekStart) + " to " + yyyymmdd(lastWeekEnd));
+
+var slowsql_data = {};
 
 var displayNames = {
  thread_type: "Thread",
@@ -52,58 +70,6 @@ function getDatabase(query) {
     return "UNKNOWN";
 }
 
-function process(raw_data) {
-    var csv = $.csv.toArrays(raw_data);
-    var totals = {}
-    var header = csv.shift();
-    header.push("total_documents");
-    header.push("occurence_rate");
-    header.push("database");
-    //header.push("key");
-    for (var i = 0; i < csv.length; i++) {
-        if (csv[i][0] == "TOTAL") {
-            // TOTAL,20140110,Fennec,29.0a1,nightly,ALL_PINGS,3515,3515,0,0
-            totals[getKey(csv[i])] = csv[i][6];
-        }
-    }
-
-    //alert(JSON.stringify(csv));
-    var csv = csv.filter(function(row){
-        //alert(row);
-        return row[0] != "TOTAL";
-    });
-
-    // Insert doc totals:
-    for (var i = 0; i < csv.length; i++) {
-        var total_docs = totals[getKey(csv[i])];
-        if (total_docs === null) {
-            total_docs = 0;
-        }
-        csv[i].push(total_docs);
-        csv[i].push(((csv[i][6] / total_docs) * 100).toFixed(2) + '%');
-        csv[i].push(getDatabase(csv[i][5]));
-        //csv[i].push(getKey(csv[i]));
-    }
-    return { totals: totals, data: csv, header: header };
-}
-
-function generateTable(csv) {
-    var html = '<table class="tablesorter data" id="slowtable"><thead><tr>';
-    for (var i = 0; i < csv.header.length; i++) {
-        html += "<th title=\"" + tooltips[csv.header[i]] + "\">" + displayNames[csv.header[i]] + "</th>";
-    }
-    html += "</tr></thead><tbody>"
-    for (var i = 0; i < csv.data.length; i++) {
-        html += "<tr>\n";
-        for (var j = 0; j < csv.data[i].length; j++) {
-            html += "<td>" + csv.data[i][j] + "</td>";
-        }
-        html += "</tr>\n";
-    }
-    html += "</tbody></table>";
-    return html;
-}
-
 function zpad(aNum) {
     return (aNum < 10 ? "0" : "") + aNum;
 }
@@ -115,98 +81,259 @@ function yyyymmdd(aDate) {
     return "" + year + zpad(month) + zpad(day);
 }
 
-function aggregate_days(per_day_data) {
-    var keys = Object.keys(per_day_data);
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        if (per_day_data[key] === false) {
-            console.log("data isn't finished yet.")
-            return;
-        }
-    };
-    console.log("aggregating some effin data!");
-    // Filter all rows by specified filters
-    // Collect rows by common query
-    // Update "this week"
-}
-
-function updateData(start, end) {
-    console.log("Updating date range: " + start + " to " + end );
-    var s = new Date(start);
-    var e = new Date(end);
-    var dates = [];
-    for (var d = new Date(s); d <= e; d.setDate(d.getDate() + 1)) {
-        var formatted = yyyymmdd(d);
-        console.log("Adding a date: " + formatted);
-        dates.push(formatted);
+function fetch_data(key, cb) {
+    if (slowsql_data[key]) {
+        cb();
+        return;
     }
-    $('#throbber').fadeIn(500);
-
-    var per_day = {};
-    dates.forEach(function(item){
-        per_day[item] = false;
-    });
-
-    dates.forEach(function(item){
-        console.log("fetching data for " + item);
-        fetch_data(per_day, item, aggregate_days);
-    });
-}
-
-function fetch_data(per_day, currentDay, callback) {
-    console.log("Current day: " + currentDay);
+    console.log("Fetching: " + key);
     var xhr = new XMLHttpRequest();
-    var url = "https://s3-us-west-2.amazonaws.com/telemetry-public-analysis/slowsql/data/slowsql" + currentDay + ".csv.gz";
+    //var url = "https://s3-us-west-2.amazonaws.com/telemetry-public-analysis/slowsql/data/weekly" + key + ".csv.gz";
+    var url = "weekly_" + key + ".csv";
+    console.log("Fetching url: " + url);
     xhr.open("GET", url, true);
     xhr.onload = function() {
         console.log("onload:" + xhr.status);
-        if (xhr.status != 200) {
+        if (xhr.status != 200 && xhr.status != 0) {
             console.log("Failed to load " + url);
-            per_day[currentDay] = {data: [], err: "failed to load " + url}
+            slowsql_data[key] = []
         } else {
             console.log("Got the data for " + url + ", processing");
             //console.log(xhr.responseText.substring(1, 50));
-            per_day[currentDay] = {data: $.csv.toArrays(xhr.responseText)};
-            console.log("done processing for " + currentDay + ", got " + per_day[currentDay].data.length + " rows");
-            callback(per_day);
-            //$('#slowsql_data').empty();
-            // $('#slowsql_data').html(generateTable(slowsql_csv));
-            // $('#slowtable').tablesorter({
-            //     //showProcessing: true,
-            //     sortList: [[11,1]],
-            //     sortInitialOrder: 'desc',
-            //     theme: 'blue',
-            //     widgets: ['filter'],
-            //     widgetOptions: {
-            //        filter_searchDelay: 600
-            //     },
-            //     //widthFixed: true,
-            // });
+            slowsql_data[key] = $.csv.toArrays(xhr.responseText);
+            console.log("done processing for " + key + ", got " + slowsql_data[key].length + " rows");
         }
         $('#throbber').fadeOut(500);
         $('#slowsql_data').fadeIn(500);
+        cb(key);
     };
     xhr.onerror = function(e) {
-        throw new Error("failed to retrieve file:" + e);
+        //throw new Error("failed to retrieve file:" + e);
+        console.log("Failed to fetch: " + url);
         $('#throbber').fadeOut(500);
         $('#slowsql_data').fadeIn(500);
+        slowsql_data[key] = []
+        cb(key);
     };
-    xhr.send(null);
+    try {
+        xhr.send(null);
+    } catch(e) {
+        console.log("Failed to fetch: " + url);
+        $('#throbber').fadeOut(500);
+        $('#slowsql_data').fadeIn(500);
+        slowsql_data[key] = []
+        cb(key);
+    }
+}
+
+function populate_this_week(key) {
+    console.log("Populating this week's data table");
+    var tbody = $('#current_data_table > tbody');
+    tbody.empty();
+    if (slowsql_data[key]) {
+        for (var i = 0; i < slowsql_data[key].length; i++) {
+            var rank = i + 1;
+            var trow = $('<tr>', {id: "tw" + rank});
+            trow.append($('<td>', {id: "tw" + rank + "rank", text: rank}));
+            var drow = slowsql_data[key][i];
+            for (var j = 0; j < drow.length; j++) {
+                trow.append($('<td>', {text: drow[j]}));
+            }
+            tbody.append(trow);
+        }
+    }
+}
+
+function populate_table(table_id, key, label) {
+    console.log("Populating " + table_id + " table");
+    var tbody = $('#' + table_id + ' > tbody');
+    tbody.empty();
+    if (!slowsql_data[key] || slowsql_data[key].length == 0) {
+        var trow = $('<tr>', {id: label + "1"});
+        trow.append($('<td>', {colspan: "6", id: label + "1rank", text: "No Data for " + key}));
+        tbody.append(trow);
+    } else {
+        var maxRows = parseInt($('#filter_rowcount').find(":selected").val());
+        for (var i = 0; i < slowsql_data[key].length; i++) {
+            if (i >= maxRows) break;
+            var rank = i + 1;
+            var trow = $('<tr>', {id: label + rank});
+            trow.append($('<td>', {id: label + rank + "rank", text: rank}));
+            var drow = slowsql_data[key][i];
+            for (var j = 0; j < drow.length; j++) {
+                trow.append($('<td>', {text: drow[j]}));
+            }
+            tbody.append(trow);
+        }
+    }
+}
+
+function update_week_over_week(lastWeekKey, thisWeekKey) {
+    var thisWeekQueryRank = {};
+    for (var i = 0; i < slowsql_data[thisWeekKey].length; i++) {
+        thisWeekQueryRank[slowsql_data[thisWeekKey][i][4]] = i+1;
+    }
+
+    var lastWeekQueryRank = {};
+    for (var i = 0; i < slowsql_data[lastWeekKey].length; i++) {
+        lastWeekQueryRank[slowsql_data[lastWeekKey][i][4]] = i+1;
+    }
+
+    var lastWeekKeys = Object.keys(lastWeekQueryRank);
+    var thisWeekKeys = Object.keys(thisWeekQueryRank);
+    for (var i = 0; i < lastWeekKeys.length; i++) {
+        var key = lastWeekKeys[i];
+        //console.log("Checking " + key);
+        if (!thisWeekQueryRank[key]) {
+            //console.log("missing in this week: " + key);
+            $('#lw' + lastWeekQueryRank[key] + "> td").addClass("missing");
+        } else if (thisWeekQueryRank[key] != lastWeekQueryRank[key]) {
+            if(thisWeekQueryRank[key] < lastWeekQueryRank[key]) {
+                //console.log("moved up this week: " + key);
+                $('#tw' + thisWeekQueryRank[key] + "> td").addClass("up");
+
+            } else if(thisWeekQueryRank[key] > lastWeekQueryRank[key]) {
+                //console.log("moved down this week: " + key);
+                $('#tw' + thisWeekQueryRank[key] + "> td").addClass("down");
+            }
+            var thisrank = thisWeekQueryRank[key];
+            var lastrank = lastWeekQueryRank[key];
+            var ranktext = thisrank + " (was " + lastrank + ")";
+            $('#tw' + thisWeekQueryRank[key] + "rank").html(ranktext);
+        } else {
+            console.log("no change: " + key);
+        }
+    }
+
+    console.log("Looking for new queries this week");
+    thisWeekKeys.forEach(function(key, idx, arr) {
+        if (!lastWeekQueryRank[key]) {
+            //console.log("new this week: " + key);
+            $('#tw' + thisWeekQueryRank[key] + "> td").addClass("new");
+        }
+    });
+}
+
+/*
+function populate_last_week(lastWeekKey) {
+    console.log("Populating last week's data table");
+
+    var thisWeekKey = "" + yyyymmdd(thisWeekStart) + "-" + yyyymmdd(thisWeekEnd);
+    var tbody = $('#previous_data_table > tbody');
+    tbody.empty();
+    var lastWeekQueryRank = {};
+    if (slowsql_data[lastWeekKey]) {
+        if (slowsql_data[lastWeekKey].length == 0) {
+            var trow = $('<tr>', {id: "lw1"});
+            trow.append($('<td>', {colspan: "6", id: "lw1rank", text: "No Data for " + lastWeekKey}));
+            tbody.append(trow);
+        }
+        var maxRows = parseInt($('#filter_rowcount').find(":selected").val());
+        console.log("limiting to " + maxRows + " rows");
+        for (var i = 0; i < slowsql_data[lastWeekKey].length; i++) {
+            var rank = i + 1;
+            if (i >= maxRows) break;
+            var trow = $('<tr>', {id: "lw" + rank});
+            trow.append($('<td>', {id: "lw" + rank + "rank", text: rank}));
+            var drow = slowsql_data[lastWeekKey][i];
+            for (var j = 0; j < drow.length; j++) {
+                trow.append($('<td>', {text: drow[j]}));
+            }
+            lastWeekQueryRank[drow[4]] = rank;
+            tbody.append(trow);
+        }
+    }
+
+    console.log("Done with last week's data table... calculating ranks");
+
+    // For each query, check it in "thisWeekKey" and update the changes accordingly.
+    var thisWeekQueryRank = {};
+    for (var i = 0; i < slowsql_data[thisWeekKey].length; i++) {
+        thisWeekQueryRank[slowsql_data[thisWeekKey][i][4]] = i+1;
+    }
+
+    var lastWeekKeys = Object.keys(lastWeekQueryRank);
+    var thisWeekKeys = Object.keys(thisWeekQueryRank);
+    for (var i = 0; i < lastWeekKeys.length; i++) {
+        var key = lastWeekKeys[i];
+        //console.log("Checking " + key);
+        if (!thisWeekQueryRank[key]) {
+            //console.log("missing in this week: " + key);
+            $('#lw' + lastWeekQueryRank[key] + "> td").addClass("missing");
+        } else if(thisWeekQueryRank[key] < lastWeekQueryRank[key]) {
+            //console.log("moved up this week: " + key);
+            $('#tw' + thisWeekQueryRank[key] + "> td").addClass("up");
+            var ranktext = $('#tw' + thisWeekQueryRank[key] + "rank").val();
+
+        } else if(thisWeekQueryRank[key] > lastWeekQueryRank[key]) {
+            //console.log("moved down this week: " + key);
+            $('#tw' + thisWeekQueryRank[key] + "> td").addClass("down");
+        } else {
+            console.log("no change: " + key);
+        }
+    }
+
+    console.log("Looking for new queries this week");
+    thisWeekKeys.forEach(function(key, idx, arr) {
+        if (!thisWeekQueryRank[key]) {
+            //console.log("new this week: " + key);
+            $('#tw' + thisWeekQueryRank[key] + "> td").addClass("new");
+        }
+    });
+}*/
+
+function get_slowsql_type() {
+    return $('input[name=slowsql_type]:radio:checked').val();
+}
+
+function get_key(start, end) {
+    return get_slowsql_type() + "_" + yyyymmdd(start) + "-" + yyyymmdd(end);
+}
+
+function update_data() {
+    $('#current_data_header').html("This Week: " + yyyymmdd(thisWeekStart) + " to " + yyyymmdd(thisWeekEnd));
+    // Update last week's header:
+    $('#previous_data_header').html("Week Of: " + yyyymmdd(lastWeekStart) + " to " + yyyymmdd(lastWeekEnd));
+
+    // Update this week's data if need be:
+    var thisWeekKey = get_key(thisWeekStart, thisWeekEnd);
+    var lastWeekKey = get_key(lastWeekStart, lastWeekEnd);
+    // Load this week's data
+    fetch_data(thisWeekKey, function(){
+        fetch_data(lastWeekKey, function() {
+            populate_table("current_data_table", thisWeekKey, "tw");
+            populate_table("previous_data_table", lastWeekKey, "lw");
+            //populate_this_week(thisWeekKey);
+            //populate_last_week(lastWeekKey);
+            update_week_over_week(lastWeekKey, thisWeekKey);
+        });
+    });
+
+    // Update last week's data if need be:
+    // var lastWeekKey = "" + yyyymmdd(lastWeekStart) + "-" + yyyymmdd(lastWeekEnd);
+    // // Load last week's data
+    // if (!slowsql_data[lastWeekKey]) {
+    //     fetch_data(lastWeekKey, populate_last_week);
+    // }
 }
 
 $(function () {
-    $(document).tooltip({delay: 1000});
-    $("#update_date_range").click(function(){
-        var start = $( "#startdate" ).val();
-        var end = $('#enddate').val();
-        updateData(start, end);
-        console.log("Updating date range to " + start + " to " + end);
+    $('#previous_week').click(function() {
+        lastWeekStart.setDate(lastWeekStart.getDate() - 7);
+        lastWeekEnd.setDate(lastWeekEnd.getDate() - 7);
+        update_data();
     });
-    $(".datepicker").datepicker({dateFormat: "yy-mm-dd", maxDate: -1});
-    $(".datepicker").datepicker("setDate", -1);
-    window.setTimeout(function () {
-        var start = $( "#startdate" ).val();
-        var end = $('#enddate').val();
-        updateData(start, end);
-    }, 100);
+    $('#next_week').click(function() {
+        lastWeekStart.setDate(lastWeekStart.getDate() + 7);
+        lastWeekEnd.setDate(lastWeekEnd.getDate() + 7);
+        update_data();
+    });
+    $('#filter_rowcount').change(update_data);
+    $('input[name=slowsql_type]').change(update_data);
+
+    update_data();
+    $('#current_data_header').html("This Week: " + yyyymmdd(thisWeekStart) + " to " + yyyymmdd(thisWeekEnd));
+    //$('#previous_data_header').html("Last Week: " + yyyymmdd(lastWeekStart) + " to " + yyyymmdd(lastWeekEnd));
+    $(document).tooltip({delay: 1000});
+
 });
